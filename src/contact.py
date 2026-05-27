a53e3811d22b60036625e2582d4435cf11a47e74
Kolesnik Constantine <kolesnikconstantine@gmail.com>
2026-05-27 16:48:44 +0300

    created contact

--- src/contact.py
+++ src/contact.py
@@ -25,3 +25,73 @@ External libs:
 
 Assignee:
 """
+from collections import UserDict
+import re
+from fields import Name, Phone, Birthday, Email, Address, Note
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones: list[Phone] = []
+        self.birthday = None 
+        self.email  = None 
+        self.address = None 
+        self.notes = []
+        
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+    
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone)) 
+
+    def edit_phone(self, old_phone, new_phone):
+         for p in self.phones:
+            if p.value == old_phone:
+                 index = self.phones.index(p)
+                 self.phones[index] = Phone(new_phone)
+                    
+    def find_phone(self, phone):
+          for p in self.phones:
+               if p.value == phone:
+                    return p.value
+    
+    def remove_phone(self, phone):
+          for p in self.phones:
+               if p.value == phone:
+                     self.phones.remove(p)
+           
+    def add_birthday(self, value):
+        self.birthday = Birthday(value)
+
+    def add_email(self, value):
+        self.email = Email(value)
+
+    def add_address(self, value):
+        self.address = Address(value)
+
+    def add_note(self, note):
+         self.notes.append(Note(note)) 
+
+    def show_note(self):
+          for p in self.notes:
+                return '; '.join(p.value for p in self.notes)
+
+
+
+class AddressBook(UserDict):
+    
+    def add_record(self, info: Record):
+        self.data[info.name.value] = info
+    
+    def find(self, name):
+         return self.data.get(name)    
+         
+    def delete(self, name):
+          if self.data[name]:
+            del self.data[name]
+            return "Record has been deleted"
+          else:
+              return "No such name in the book"
+
+    def get_upcoming_birthdays(self, days):
+         pass
