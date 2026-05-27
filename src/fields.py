f21bd645a07e302a5e84dadddb31b0a880eac224
Kolesnik Constantine <kolesnikconstantine@gmail.com>
2026-05-27 15:47:37 +0300

    created fields

--- src/fields.py
+++ src/fields.py
@@ -25,3 +25,54 @@ External libs:
 
 Assignee:
 """
+from datetime import datetime
+import re
+from abc import ABC, abstractmethod
+
+#validate() ??? ABC @abstractmethod 
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+		pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+            pattern = r"^\+?\d{10,14}$"
+            match = re.search(pattern, value)
+            if match:
+                  self.value = value
+            else:
+                  raise Exception("Phone must be E.164-compatible, 10-15 digits, optional leading +")
+         
+
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            self.value = datetime.strptime(value, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+
+class Email(Field):
+    def __init__(self, value):
+        pattern = r"\w+@\w+\.\w+"
+        match = re.search(pattern, value)
+        if match:
+            self.value = match.group()
+        else:
+             raise ValueError("Invalid email format.")
+
+
+class Address(Field):
+    pass
+
+
+class Tag(Field):
+    pass
